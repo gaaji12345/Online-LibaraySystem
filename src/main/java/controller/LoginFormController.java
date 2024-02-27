af270dbea96fc53e618449c702aade807af07d08
gaji12345 <gajimax12345@gmail.com>
2024-02-27 14:09:28 +0530

    first commit

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    public AnchorPane root;
+    public AnchorPane mainPain;
+    public AnchorPane subPain;
+    public TextField txtUsername;
+    public PasswordField txtPasswordField;
+    public TextField txtPassword;
+    public Button btnLogin;
+    public CheckBox chkBox;
+}
