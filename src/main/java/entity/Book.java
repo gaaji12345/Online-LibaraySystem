cfcad82fe0a5d18adffb8b822c82ccc7a7b62c2b
gaji12345 <gajimax12345@gmail.com>
2024-02-28 09:22:42 +0530

    create bookdto and book entity

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,68 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @Column(length = 20)
+    private String bId;
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(String bId, String title, String author, String genre, String status) {
+        this.setbId(bId);
+        this.setTitle(title);
+        this.setAuthor(author);
+        this.setGenre(genre);
+        this.setStatus(status);
+    }
+
+    private String status;
+
+    public String getbId() {
+        return bId;
+    }
+
+    public void setbId(String bId) {
+        this.bId = bId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
